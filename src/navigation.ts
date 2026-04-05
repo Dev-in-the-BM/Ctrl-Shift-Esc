059a798068b2ad4ebe3ac391537cf1da6a77210e
Dev-in-the-BM <devinthebm@outlook.com>
2026-04-05 17:16:05 -0400

    updated footer, removed some social links, added reddit and jtech links, modified credits

--- src/navigation.ts
+++ src/navigation.ts
@@ -16,13 +16,12 @@ export const footerData = {
     { text: 'Privacy Policy', href: getPermalink('/privacy') },
   ],
   socialLinks: [
-    { ariaLabel: 'X', icon: 'tabler:brand-x', href: '#' },
-    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
-    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+    { ariaLabel: 'Reddit', icon: 'tabler:brand-reddit', href: 'https://www.reddit.com/user/Dev-in-the-Bm/' },
+    { ariaLabel: 'Jtech Forums', text: '<img src="https://github.com/Dev-in-the-BM/Dev-in-the-BM.github.io/blob/main/Assets/Jtech%20logo%20cropped.png?raw=true" alt="Jtech Forums logo" class="w-5 h-5 opacity-65 invert dark:invert-0" />', href: 'https://forums.jtechforums.org/u/dev-in-the-bm_2.0/' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
     { ariaLabel: 'Github', icon: 'tabler:brand-github', href: 'https://github.com/Dev-in-the-BM/AstroAuthor/' },
   ],
   footNote: `
-    Made by <a class="text-blue-600 dark:text-muted" href="https://github.com/Dev-in-the-BM"> Dev-in-the-BM</a> and <a class="text-blue-600 dark:text-muted" href="https://github.com/arthelokyo"> Arthelokyo</a> · All rights reserved.
+    Made by <a class="text-blue-600 dark:text-muted" href="https://github.com/Dev-in-the-BM"> Dev-in-the-BM</a> · All rights reserved.
   `,
 };
